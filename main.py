353ba3bb2c9fb1a96e8304a9d05c2c455e97d007
Marce <benitezquinteroginamarcela@gmail.com>
2025-12-09 13:07:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def main():
-  print("Hello learners!")
-
-if __name__=="__main__":
-  main()
+import json
+import requests
+response = requests.get("https://api-colombia.com/api/v1/TypicalDish")
+Typical_disks =response.json()
+input("Ingresa un numero, para ver un plato tipico:")
+for Typical_disk in Typical_disks:
+  print (Typical_disk["name"])
+  print (Typical_disk["description"])
+  print ("*"*40)
